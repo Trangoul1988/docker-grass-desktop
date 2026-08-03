72b9ab8c146fb7d1e4c9392a0997faf5cef49b87
Trangoul1988 <96128773+Trangoul1988@users.noreply.github.com>
2026-08-03 08:25:12 +0200

    updated grass version 7.5.4

--- start.sh
+++ start.sh
@@ -43,26 +43,6 @@ if ! [ -f ~/.grass-configured ]; then
 
     # Handle grass login
     xdotool search --name Grass | tail -n1 | xargs xdotool windowfocus
-    sleep 10
-    xdotool key Tab
-    xdotool key Tab
-    sleep 5
-    xdotool type "$GRASS_USERNAME"
-    xdotool key Tab
-    xdotool key Return
-    sleep 5
-    xdotool mousemove 600 535 click 1
-    sleep 5
-    xdotool key Tab
-    xdotool key Tab
-    xdotool key Tab
-    xdotool key Tab
-    xdotool type "$GRASS_PASSWORD"
-    xdotool key Tab
-    xdotool key Tab
-    xdotool key Return
-    sleep 10
-    xdotool key Escape
 
     touch ~/.grass-configured
 fi
